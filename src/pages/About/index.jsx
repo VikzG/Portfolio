833071851df213adc8e5553a0596b20e894f36e1
VikzG <bouzemanej@yahoo.com>
2023-08-10 13:25:07 +0200

    maj pf

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -3,9 +3,16 @@ import Navigation from "../../components/Navigation";
 import Header from "../../components/Header";
 import myPhoto from "../../assets/photo_portfolio.jpg";
 import Footer from "../../components/Footer";
+import SimpleParallax from 'simple-parallax-js';
 
 function About() {
-  
+  let aboutPhoto = document.getElementsByClassName('my-photo')
+  new SimpleParallax(aboutPhoto,
+    {
+      scale: 1.2,
+      overflow: true,
+    }
+    );
 
       return (
         <div className = "about">
@@ -14,7 +21,7 @@ function About() {
         <div className='photo-presentation'>
           <span className='presentation-words animate__animated animate__fadeIn'>"DESIGN IS THE VISIBLE REFLECTION<br/>OF THE INVISIBLE THINKING."</span>
         <div className="my-photo-container animate__animated presentation-words animate__fadeIn">
-        <img src={myPhoto} alt="jeremy" />
+        <img className = "my-photo" src={myPhoto} alt="my photo" />
         </div>
         <span className='animate__animated presentation-words animate__fadeIn'>"CODE IS POETRY IN MOTION."</span>
         </div>
